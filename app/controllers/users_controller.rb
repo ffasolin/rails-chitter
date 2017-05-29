46cdd5789376d5d0abc5a39ac8c4a7b9d56a75a1
Bailey Kursar <bkursar@gmail.com>
2017-05-29 15:42:16 +0100

    Adding a user form that works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(params.require(:users).permit(:name, :username, :email, :password))
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
+  end
 end
