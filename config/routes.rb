5325f35bbe7cae15134586030166a60903ae822a
Bailey Kursar <bkursar@gmail.com>
2017-05-29 15:56:02 +0100

    Made /cheets show the feed of cheets

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
-
+  # Hey future Bailey! You are awesome. Remember, this is for the feed:
+  get '/cheets' => 'cheets#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
